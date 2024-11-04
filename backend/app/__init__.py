8a525d30d4b439e220e9c2bf51b03202140de56a
zakariahka <102551487+zakariahka@users.noreply.github.com>
2024-11-04 15:21:01 -0500

    defined user model

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,10 +2,15 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    db = SQLAlchemy(app)
+    db.init_app(app)
+
+    with app.app_context():
+        from . import models
 
     from .blueprints.user import user_routes as user_blueprint
     app.register_blueprint(user_blueprint, url_prefix='/user')
--- backend/app/blueprints/user/user_routes.py
+++ backend/app/blueprints/user/user_routes.py
@@ -2,4 +2,5 @@ from . import user
 
 @user.route('/signup', methods=['POST'])
 def signup():
-    return 
+    
+    return
