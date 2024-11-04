0cdfbbf16d76a8de3f9680003fc56ae3684bb9f5
zakariahka <102551487+zakariahka@users.noreply.github.com>
2024-11-04 14:04:48 -0500

    registering user blueprints

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,10 @@
 from flask import Flask
-import os
+from config import Config
 
-def create_app()
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    from .blueprints.user import user_routes as user_blueprint
+    app.register_blueprint(user_blueprint, url_prefix='/user')
+    
