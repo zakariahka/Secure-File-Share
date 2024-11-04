0682dd8405ade265507b7e5d53727c5a694d4c40
zakariahka <102551487+zakariahka@users.noreply.github.com>
2024-11-04 14:52:52 -0500

    initialized db

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    db = SQLAlchemy(app)
 
     from .blueprints.user import user_routes as user_blueprint
     app.register_blueprint(user_blueprint, url_prefix='/user')
