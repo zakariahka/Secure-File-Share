934985c27544eb397c7c5f46b4da2f0e3d38a1fb
zakariahka <102551487+zakariahka@users.noreply.github.com>
2025-01-07 15:33:46 -0500

    logging current env during run time

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,15 @@
 from app import create_app
+import os
+from dotenv import load_dotenv
+import logging
 
+load_dotenv()
 app = create_app()
+env = os.getenv("env")
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.info(f"This app is running in {env} environment")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
