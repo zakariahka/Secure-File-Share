dc5e25e25d53105bafc6c475433dbe4e62d42391
zakariahka <102551487+zakariahka@users.noreply.github.com>
2025-01-07 19:01:42 -0500

    created model for files, and user relationship

--- backend/app/models.py
+++ backend/app/models.py
@@ -6,8 +6,10 @@ class User(db.Model):
 
     id = db.Column(db.Integer(), primary_key=True)
     email = db.Column(db.String(120), nullable=False, unique=True)
-    name = db.Column(db.String(50), nullable=False, unique=False)
-    password = db.Column(db.String(255), nullable=False, unique=False)
+    name = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    files = db.relationship("File", back_populates="user", cascade="all, delete-orphan")
 
     def __init__(self, email, name, password):
         self.email = email
@@ -15,4 +17,31 @@ class User(db.Model):
         self.password = password
 
     def to_dict(self):
-        return {"id": self.id, "email": self.email, "name": self.name}
+        return {
+            "id": self.id,
+            "email": self.email,
+            "name": self.name,
+            "files": [file.to_dict() for file in self.files],
+        }
+    
+class File(db.Model):
+    __tablename__ = "files"
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    encrypted_file = db.Column(db.LargeBinary, nullable=False)
+    user_id = db.Column(db.Integer(), db.ForeignKey("users.id"), nullable=False)
+
+    user = db.relationship("User", back_populates="files")
+
+    def __init__(self, name, encrypted_file, user_id):
+        self.name = name
+        self.encrypted_file = encrypted_file
+        self.user_id = user_id
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "user_id": self.user_id,
+        }
