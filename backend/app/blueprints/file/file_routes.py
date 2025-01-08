d71f0d06c1df88fe82e48a505fc398142edc6fc5
zakariahka <102551487+zakariahka@users.noreply.github.com>
2025-01-07 19:02:28 -0500

    storing encrypted file to respective users table

--- backend/app/blueprints/file/file_routes.py
+++ backend/app/blueprints/file/file_routes.py
@@ -2,10 +2,18 @@ from . import file_bp
 from flask import jsonify, request, make_response, current_app
 from cryptography.fernet import Fernet
 import os
+from app import db
+from flask_jwt_extended import jwt_required, decode_token
+import jwt
+from app.models import File, User
 
 @file_bp.route("/encrypt", methods=["POST"])
 def encrypt():
     key = current_app.config["ENCRYPTION_KEY"]
+
+    token = request.cookies.get("token")
+    if not token:
+        return jsonify({"error": "Unauthorized"}), 401
 
     if 'file' not in request.files:
         return jsonify({"error": "No file part"}), 400
@@ -18,9 +26,28 @@ def encrypt():
     file_content = file.read()
     
     fernet = Fernet(key)
-    encrypted_file = fernet.encrypt(file_content)
+    encrypted_content = fernet.encrypt(file_content)
+
+    decoded_token = jwt.decode(token, current_app.config["JWT_SECRET"], algorithms=["HS256"])
+    user_id = decoded_token["id"]
+
+    encrypted_file = File(name=file.filename, encrypted_file=encrypted_content, user_id=user_id)
+
+    try:
+        db.session.add(encrypted_file)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": "Database error", "details": str(e)}), 500
+
 
     return jsonify({
         "message": "File encrypted successfully",
-        "encrypted_content": encrypted_file.decode("utf-8")
+        "file_id": encrypted_file.id,
+        "encrypted_content": encrypted_content.decode("utf-8"),
+        "user_id": user_id
     }), 200
+
+@file_bp.route("/decrypt", methods=["GET"])
+def decrypt():
+    return None
