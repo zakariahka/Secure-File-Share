efce8b6bc851230b82ce6d363a51705318474679
zakariahka <102551487+zakariahka@users.noreply.github.com>
2025-01-10 19:16:09 -0500

    removing unnecessary env variables

--- backend/config.py
+++ backend/config.py
@@ -10,11 +10,9 @@ class DevelopmentConfig:
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     CLIENT_URL = os.getenv("CLIENT_URL")
     ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY")
-    PORT = int(os.getenv("DEV_PORT", 5001))
+    PORT = int(os.getenv("DEV_PORT"))
     AES_KEY = os.getenv("AES_KEY")
-    HMAC_KEY = os.getenv("HMAC_KEY")
     JWT_TOKEN_LOCATION = ["cookies"]
-    JWT_ACCESS_COOKIE_NAME = "token"
     JWT_COOKIE_CSRF_PROTECT = False
 
 class ProductionConfig:
@@ -23,13 +21,11 @@ class ProductionConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY")
-    PORT = int(os.getenv("PROD_PORT", 5001))
+    PORT = int(os.getenv("PROD_PORT"))
     AES_KEY = os.getenv("AES_KEY")
-    HMAC_KEY = os.getenv("HMAC_KEY")
     JWT_TOKEN_LOCATION = ["cookies"]
-    JWT_ACCESS_COOKIE_NAME =  "token"
     JWT_COOKIE_CSRF_PROTECT = False
-    
+
 class TestConfig:
     SECRET_KEY = os.getenv("SECRET_KEY")
     TESTING = True
@@ -37,8 +33,7 @@ class TestConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY")
-    PORT = int(os.getenv("TEST_PORT", 5001))
+    PORT = int(os.getenv("TEST_PORT"))
     AES_KEY = os.getenv("AES_KEY")
-    HMAC_KEY = os.getenv("HMAC_KEY")
     JWT_TOKEN_LOCATION = ["cookies"]
     JWT_COOKIE_CSRF_PROTECT = False
