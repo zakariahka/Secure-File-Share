508e852a4aef78731c75e3dcd8e1d4b3093dc3be
zakariahka <102551487+zakariahka@users.noreply.github.com>
2025-01-13 13:39:30 -0500

    creating test user for files

--- backend/tests/test_files.py
+++ backend/tests/test_files.py
@@ -15,3 +15,27 @@ def client():
         yield app.test_client()
         db.session.remove()
         db.drop_all()
+
+def random_word_generator():
+    return ''.join(random.choices(string.ascii_letters, k=8))
+
+def random_email_generator():
+    username = random_word_generator()
+    domain = random.choice(['gmail', 'outlook', 'icloud', 'hotmail', 'yahoo', 'aol'])
+    return f'{username}@{domain}.com'
+
+@pytest.fixture
+def test_user():
+    email = random_email_generator()
+    password = random_word_generator()
+    hashed_password = generate_password_hash(password) 
+    user = User(email=email, name=random_word_generator(), password=hashed_password)
+    
+    db.session.add(user)
+    db.session.commit()
+    
+    return {
+        "id": user.id,
+        "email": email,
+        "password": password,
+    }
