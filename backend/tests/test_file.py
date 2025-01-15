f128ef4697001d73ea776b1f195e40c623a9fdaa
zakariahka <102551487+zakariahka@users.noreply.github.com>
2025-01-15 16:53:34 -0500

    testing encryption while  unauthorized

---
+++ backend/tests/test_file.py
@@ -0,0 +1,47 @@
+import pytest
+from app import create_app, db
+from app.models import User
+import random
+import string
+from werkzeug.security import generate_password_hash 
+import os
+
+@pytest.fixture
+def client():
+    app = create_app()
+    os.environ["env"] = "testing" 
+    with app.app_context():
+        db.create_all()
+        yield app.test_client()
+        db.session.remove()
+        db.drop_all()
+
+def random_word_generator():
+    return ''.join(random.choices(string.ascii_letters, k=8))
+
+def random_email_generator():
+    username = random_word_generator()
+    domain = random.choice(['gmail', 'outlook', 'icloud', 'hotmail', 'yahoo', 'aol'])
+    return f'{username}@{domain}.com'
+
+@pytest.fixture
+def test_user():
+    email = random_email_generator()
+    password = random_word_generator()
+    hashed_password = generate_password_hash(password) 
+    user = User(email=email, name=random_word_generator(), password=hashed_password)
+    
+    db.session.add(user)
+    db.session.commit()
+    
+    return {
+        "id": user.id,
+        "email": email,
+        "password": password,
+    }
+
+def test_unauthorized(client):
+    request = client.post('file/encrypt')
+
+    assert request.status_code == 401
+    assert request.get_json()["msg"] == "Missing cookie \"access_token_cookie\""
