be0c894a4c87d8bffbe63082246a1332ca405999
zakariahka <102551487+zakariahka@users.noreply.github.com>
2025-04-18 19:41:32 -0400

    Changed db urls

--- backend/config.py
+++ backend/config.py
@@ -3,9 +3,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+class ProductionConfig:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY")
+    PORT = int(os.getenv("PROD_PORT"))
+    AES_KEY = os.getenv("AES_KEY")
+    JWT_TOKEN_LOCATION = ["cookies"]
+    JWT_COOKIE_CSRF_PROTECT = False
+
 class DevelopmentConfig:
     SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DEV_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     CLIENT_URL = os.getenv("CLIENT_URL")
@@ -15,21 +26,10 @@ class DevelopmentConfig:
     JWT_TOKEN_LOCATION = ["cookies"]
     JWT_COOKIE_CSRF_PROTECT = False
 
-class ProductionConfig:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-    ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY")
-    PORT = int(os.getenv("PROD_PORT"))
-    AES_KEY = os.getenv("AES_KEY")
-    JWT_TOKEN_LOCATION = ["cookies"]
-    JWT_COOKIE_CSRF_PROTECT = False
-
 class TestConfig:
     SECRET_KEY = os.getenv("SECRET_KEY")
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.getenv("TEST_SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.getenv("TEST_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY")
